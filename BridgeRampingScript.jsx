642b44bd99e5b7e2bace7f6cb764d08d827fa951
David Milligan <david.l.milligan@gmail.com>
2013-10-18 23:13:18 -0400

    Added Copyright

--- BridgeRampingScript.jsx
+++ BridgeRampingScript.jsx
@@ -1,5 +1,18 @@
 ﻿#target bridge
 
+/* Copyright (C) 2013 David Milligan
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 3
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+
 function BrRamp()
 {
     this.requiredContext = "\tAdobe Bridge must be running.\n\tExecute against Bridge as the Target.\n";

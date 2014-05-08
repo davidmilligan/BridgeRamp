55d07f6a5b77bf4506a84c58ac56fc1b1f86fecf
PaulJBis <paulj@fastmail.es>
2014-05-08 16:24:24 +0200

    Updated copyright notice.

--- BridgeRampingScript.jsx
+++ BridgeRampingScript.jsx
@@ -2,6 +2,8 @@
 
 /* Copyright (C) 2013 David Milligan
  *
+ * With additions (C) 2014 by Paulo Jan
+ *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
  * as published by the Free Software Foundation; either version 3
